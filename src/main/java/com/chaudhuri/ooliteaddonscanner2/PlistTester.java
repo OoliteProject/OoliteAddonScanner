1af975079a8f1eeac5211f337cbd6dbe3464bbd8
hiran <hiran@homeserver.indernet>
2021-10-13 22:29:37 +0200

    Added unit test

--- src/main/java/com/chaudhuri/ooliteaddonscanner2/PlistTester.java
+++ src/main/java/com/chaudhuri/ooliteaddonscanner2/PlistTester.java
@@ -13,16 +13,33 @@ import java.nio.charset.StandardCharsets;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author hiran
  */
 public class PlistTester {
+    private static final Logger log = LoggerFactory.getLogger(PlistTester.class);
 
-    private static final String plistFilePath = "/home/hiran/GNUstep/Library/Caches/org.aegidian.oolite/Oolite-manifests.plist";
+    //private static final String plistFilePath = "/home/hiran/GNUstep/Library/Caches/org.aegidian.oolite/Oolite-manifests.plist";
+    private static final String plistFilePath = "src/test/data/cholmondely.plist";
+    
+    private static void dumpPc(PlistParser.ParseContext pc, String prefix) {
+        System.out.print(prefix); System.out.println(pc.getClass().getName());
+        for (ParseTree pt: pc.children) {
+            dumpPT(pt, prefix+"  ");
+        }
+    }
+
+    private static void dumpPT(ParseTree pt, String prefix) {
+        System.out.print(prefix); System.out.println(pt.getClass().getName());
+    }
 
     public static void main(String[] args) throws Exception {
+        log.debug("main(...)");
         
         InputStream in = new FileInputStream(plistFilePath);
         ReadableByteChannel channel = Channels.newChannel(in);
@@ -32,6 +49,10 @@ public class PlistTester {
         
         CommonTokenStream tokenStream = new CommonTokenStream(lexer);
         PlistParser parser = new PlistParser(tokenStream);
-        parser.parse();
+        PlistParser.ParseContext pc = parser.parse();
+        
+        log.debug("Parsed plist in {}", plistFilePath);
+        
+        dumpPc(pc, "");
     }
 }
