6e94eddbd830e67b746afdd7661b7efef54f3229
hiran <hiran@homeserver.indernet>
2023-02-08 23:21:26 +0100

    Add startup scripts and have a Linux build

--- src/main/resources-ext/run-linux.sh
+++ src/main/resources-ext/run-linux.sh
@@ -3,16 +3,10 @@
 ADDONSCANNER_HOME=`dirname $0`
 JAVA_OPTS=""
 
-if [ ! -f "configuration.properties" ]
-then
-	pushd .
-	cd "${ADDONSCANNER_HOME}"
-	java ${JAVA_OPTS} -jar dist/@projectname@-@pomversion@.jar $@
-	RETVAL=$?
-	popd
-else
-	java ${JAVA_OPTS} -jar ${ADDONSCANNER_HOME}/dist/@projectname@-@pomversion@.jar $@
-	RETVAL=$?
-fi
+pushd .
+cd "${ADDONSCANNER_HOME}"
+java ${JAVA_OPTS} -jar dist/@projectname@-@pomversion@ $@
+RETVAL=$?
+popd
 
 exit $RETVAL
