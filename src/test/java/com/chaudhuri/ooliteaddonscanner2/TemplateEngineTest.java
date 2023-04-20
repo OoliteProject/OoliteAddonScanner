7e363cb7378756927c9ba64e7f3f8de78445c5cc
hiran <hiran@homeserver.indernet>
2023-04-20 08:21:41 +0200

    Fixed SonarQube/Checkstyle-detected issues

--- src/test/java/com/chaudhuri/ooliteaddonscanner2/TemplateEngineTest.java
+++ src/test/java/com/chaudhuri/ooliteaddonscanner2/TemplateEngineTest.java
@@ -2,7 +2,12 @@
  */
 package com.chaudhuri.ooliteaddonscanner2;
 
+import com.chaudhuri.ooliteaddonscanner2.model.Expansion;
+import com.chaudhuri.ooliteaddonscanner2.model.Ship;
+import freemarker.template.TemplateNotFoundException;
 import java.io.File;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author hiran
  */
 public class TemplateEngineTest {
+    private static final Logger log = LogManager.getLogger();
     
     public TemplateEngineTest() {
     }
@@ -40,7 +46,32 @@ public class TemplateEngineTest {
      */
     @Test
     public void testProcess() throws Exception {
-        System.out.println("process");
+        log.info("process");
+        
+        TemplateEngine engine = new TemplateEngine();
+        File tmpDir = new File("target/test/TemplateEngineTest");
+        tmpDir.mkdirs();
+        File outFile = File.createTempFile("ship", ".html", tmpDir);
+        outFile.delete();
+        
+        Expansion expansion = new Expansion();
+        expansion.setIdentifier("myexpansion");
+        expansion.setTitle("myexpansion-title");
+        Ship data = new Ship();
+        data.setIdentifier("myship");
+        data.setExpansion(expansion);
+        
+        log.debug("writing {}", outFile);
+
+        assertFalse(outFile.exists());
+        try {
+            engine.process(data, "ship", outFile);
+            fail("How come the template was found?");
+        } catch (TemplateNotFoundException e) {
+            log.debug("caught expected exception", e);
+        }
+        engine.process(data, "ship.ftlh", outFile);
+        assertTrue(outFile.exists());
     }
     
 }
